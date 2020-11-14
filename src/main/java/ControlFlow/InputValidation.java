a2f58a0c6ad18ac19448d726da56a3dfe9e519d1
Jared Langson <langson.jared@gmail.com>
2020-11-14 15:21:42 -0500

    Added check for between1And50

--- src/main/java/ControlFlow/InputValidation.java
+++ src/main/java/ControlFlow/InputValidation.java
@@ -11,6 +11,7 @@ import java.util.function.*;
  * @author jared
  */
 public class InputValidation {
+    public static String[] testInputs = {"yes", "no", "y", "n", "YES", "NO", "123", "85.0", "-1", "0", "d8($()*", "1988", "32", "50.0", "50.00", "50.01", "50.1", "09", "12", "200", "100", "35.33", "", "\n", "\t"}; 
     
     //true for all variations of y yes n and no
     public static boolean isYesNo (String response){
@@ -51,12 +52,23 @@ public class InputValidation {
         return isInt(str) && isTwoChars(str) && Integer.parseInt(str) > -1;
     }
     
-    public static String[] testInputs = {"yes", "no", "y", "n", "YES", "NO", "123", "85.0", "-1", "0", "d8($()*", "1988", "32", "09", "12", "200", "100", "35.33", "", "\n", "\t"}; 
-    
+    //if the number is between 1 and 50 inclusive return true
+    public static boolean between1and50(String str){
+        //uses double not int because otherwise 3.5 would return false. 
+        //but in implementation of other methods, all numbers are treated as ints. 
+        try{
+            double d = Double.parseDouble(str);
+            return d >= 1.0 && d <= 50.0;
+        }
+        catch(NumberFormatException nfe){
+            return false;
+        }
+    }
+   
+    //this method calls all the above boolean methods on the testInputs string array. 
    public static void runTests(){
     for(String input : testInputs){
-        System.out.println(input+ "\tisYesNo?\t" + isYesNo(input) +"\tisInt?\t" + isInt(input) +"\tisTwoChars\t" + isTwoChars(input) + "\tisQBJersey\t" + isQBJersey(input) + "\tisCarModel\t" + isCarModel(input));
-   
+        System.out.println(input+ "\tisYesNo?\t" + isYesNo(input) +"\tisInt?\t" + isInt(input) +"\tisTwoChars\t" + isTwoChars(input) + "\tisQBJersey\t" + isQBJersey(input) + "\tisCarModel\t" + isCarModel(input) +"\tb/t1.50\t" + between1and50(input));
         }
    } 
 }
