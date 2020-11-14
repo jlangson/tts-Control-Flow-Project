b4616200ce97b9a6274b51457377d0ddd586050c
Jared Langson <langson.jared@gmail.com>
2020-11-14 15:11:42 -0500

    Added isCarModel

--- src/main/java/ControlFlow/InputValidation.java
+++ src/main/java/ControlFlow/InputValidation.java
@@ -41,16 +41,21 @@ public class InputValidation {
         if(isInt(str)){
             int n = Integer.parseInt(str);
             //1 to 19 are current QB jersey positions. 0 has ben used historically https://en.wikipedia.org/wiki/National_Football_League_uniform_numbers#Current_system
-            return n >=0 && n<=19; 
+            return isInt(str) && n >=0 && n<=19; 
         }
         return false;
     }
     
-    public static String[] testInputs = {"yes", "no", "y", "n", "YES", "NO", "123", "85.0", "-1", "0", "d8($()*", "1988", "09", "200", "100", "35.33", "", "\n", "\t"}; 
+    public static boolean isCarModel(String str){
+        //short circuit evaluation means parseInt won't trigger an error if it's not parseable.
+        return isInt(str) && isTwoChars(str) && Integer.parseInt(str) > -1;
+    }
+    
+    public static String[] testInputs = {"yes", "no", "y", "n", "YES", "NO", "123", "85.0", "-1", "0", "d8($()*", "1988", "32", "09", "12", "200", "100", "35.33", "", "\n", "\t"}; 
     
    public static void runTests(){
     for(String input : testInputs){
-        System.out.println(input+ "\tisYesNo?\t" + isYesNo(input) +"\tisInt?\t" + isInt(input) +"\tisTwoChars\t" + isTwoChars(input) + "\tisQBJersey\t" + isQBJersey(input));
+        System.out.println(input+ "\tisYesNo?\t" + isYesNo(input) +"\tisInt?\t" + isInt(input) +"\tisTwoChars\t" + isTwoChars(input) + "\tisQBJersey\t" + isQBJersey(input) + "\tisCarModel\t" + isCarModel(input));
    
         }
    } 
